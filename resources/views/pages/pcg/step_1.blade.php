fdf5e7aee4aa8202b493559f0d1e4e256b0d6c58
capogan <ogan@capioteknologi.co.id>
2021-03-22 18:23:12 +0700

    api

--- resources/views/pages/pcg/step_1.blade.php
+++ resources/views/pages/pcg/step_1.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12 col-sm-12  ">
         <div class="x_panel">
             <div class="x_title">
-                <h2><i class="fa fa-bars"></i> Tabs <small>Float right</small></h2>
+                <h2>Detail Faktur</h2>
                 <ul class="nav navbar-right panel_toolbox">
                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                     </li>
