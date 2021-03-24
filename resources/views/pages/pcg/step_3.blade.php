51363874d561b5c2f6f51b45a28463b63d579765
ctridcat <ridcat@cashtree.id>
2021-03-24 20:25:16 +0700

    ridcat

--- resources/views/pages/pcg/step_3.blade.php
+++ resources/views/pages/pcg/step_3.blade.php
@@ -173,9 +173,9 @@
             </div>
         </div>
         <a href="step-2"><button class="btn btn-bd-download pull-left"><i class="fa fa-arrow-left"></i> Kembali</button></a>
-        @if($score['message']['credit_limit'] > 1000)
-            <button class="btn btn-primary pull-right" id="btn_send_loan"><i class="fa fa-arrow-right"></i> Setujui</button>
-        @endif
+        
+            <button class="btn btn-primary pull-right" id="btn_send_loan" style="display:none"><i class="fa fa-arrow-right"></i> Setujui</button>
+        
         <button class="btn btn-danger pull-right" id="btn_reject"><i class="fa fa-close"></i> Tolak Pengajuan</button>
     </div>
 
