267ebd8867b935bb217928162d964706a7d58663
capogan <ogan@capioteknologi.co.id>
2021-03-30 23:53:56 +0700

    admin

--- public/js/verification_final.js
+++ public/js/verification_final.js
@@ -1,84 +1,87 @@
-$('#btn_submit_crm').click(function(){
-    bootbox.dialog({
-        message: "Anda akan menyetujui peminjaman ini dan meneruskan ke verifikasi akhir",
-        title: "Perhatian",
-        buttons: {
-            success: {
-                label: "Ya, Saya setuju",
-                className: "btn-primary",
-                callback: function() {
-                    $('#crm_form').submit();
-                }
-            },
+$( document ).ready(function() {
+    $('#btn_submit_crm').click(function(){
+        bootbox.dialog({
+            message: "Anda akan menyetujui peminjaman ini dan meneruskan ke verifikasi akhir",
+            title: "Perhatian",
+            buttons: {
+                success: {
+                    label: "Ya, Saya setuju",
+                    className: "btn-primary",
+                    callback: function() {
+                        $('#crm_form').submit();
+                    }
+                },
 
-        }
+            }
+        });
+    });
+
+    $("#crm_form").on("submit", function(event) {
+
+        event.preventDefault();
+
+        var btn = $("#btn_submit_voucher");
+        btn.attr("disabled", "disabled");
+
+        var token = $('meta[name="csrf-token"]').attr('content');
+
+        $.ajax({
+            url: '/loan/verification/phone',
+            method:"POST",
+            headers: {
+                'X-CSRF-TOKEN': token
+            },
+            async:true,
+            data:new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success:function(response)
+            {
+                var text = '';
+                var res = JSON.parse(response);
+                if(res.status) {
+                    bootbox.alert({
+                        title: "Berhasil!",
+                        message: "<i data-feather='check'></i> "+res.message,
+                        centerVertical:true,
+                        onShow: function(e) {
+                            feather.replace();
+                        },
+                        callback: function() {
+                            btn.removeAttr("disabled");
+                        },
+                        onHide: function(e) {
+                        },
+                    });
+                    location.reload();
+                }else{
+                    // close_loading();
+                    $.each(res.message, function( index, value ) {
+                        text += '<p class="error"><i data-feather="x-square"></i> '+ value[0]+'</p>';
+                    });
+                    bootbox.alert({
+                        title: "Data Tidak lengkap!",
+                        message: text,
+                        centerVertical:true,
+                        onShow: function(e) {
+                            feather.replace();
+                        },
+                        callback: function() {
+                            btn.removeAttr("disabled");
+                        },
+                        onHide: function(e) {
+                            window.location = "/product"
+                        },
+
+                    });
+                    // $(".result-message").addClass('alert alert-error').html(text)
+                }
+            }
+        })
     });
 });
 
-$("#crm_form").on("submit", function(event) {
-
-    event.preventDefault();
-
-    var btn = $("#btn_submit_voucher");
-    btn.attr("disabled", "disabled");
-
-    var token = $('meta[name="csrf-token"]').attr('content');
-
-    $.ajax({
-        url: '/loan/verification/phone',
-        method:"POST",
-        headers: {
-            'X-CSRF-TOKEN': token
-        },
-        async:true,
-        data:new FormData(this),
-        contentType: false,
-        cache: false,
-        processData: false,
-        success:function(response)
-        {
-            var text = '';
-            var res = JSON.parse(response);
-            if(res.status) {
-                bootbox.alert({
-                    title: "Berhasil!",
-                    message: "<i data-feather='check'></i> "+res.message,
-                    centerVertical:true,
-                    onShow: function(e) {
-                        feather.replace();
-                    },
-                    callback: function() {
-                        btn.removeAttr("disabled");
-                    },
-                    onHide: function(e) {
-                    },
-                });
-                location.reload();
-            }else{
-                // close_loading();
-                $.each(res.message, function( index, value ) {
-                    text += '<p class="error"><i data-feather="x-square"></i> '+ value[0]+'</p>';
-                });
-                bootbox.alert({
-                    title: "Data Tidak lengkap!",
-                    message: text,
-                    centerVertical:true,
-                    onShow: function(e) {
-                        feather.replace();
-                    },
-                    callback: function() {
-                        btn.removeAttr("disabled");
-                    },
-                    onHide: function(e) {
-                        window.location = "/product"
-                    },
-
-                });
-                // $(".result-message").addClass('alert alert-error').html(text)
-            }
-        }
-    })
-});
 
 function init_data_table(){
     let table = $('#table_verification');
