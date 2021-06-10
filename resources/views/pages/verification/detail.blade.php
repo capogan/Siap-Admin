9fa986a892e33244ec4d8e995f44757f8712c1be
capogan <ogan@capioteknologi.co.id>
2021-06-10 16:44:09 +0700

    verification final

--- resources/views/pages/verification/detail.blade.php
+++ resources/views/pages/verification/detail.blade.php
@@ -13,6 +13,30 @@
                 <div class="x_content">
 {{--                    Upload Document--}}
                     <div class="tab-content" id="myTabContent">
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 ">
+                                <div class="x_panel">
+                                    <div class="x_title">
+                                        <h2>Credit Scoring</h2>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="x_content">
+
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="card-box table-responsive">
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="ln_solid"></div>
+                            </div>
+                        </div>
+
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="x_panel">
