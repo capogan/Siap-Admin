26f944bdddec0263318c6a7e446dd3dea3fbc601
capogan <ogan@capioteknologi.co.id>
2021-06-21 00:08:16 +0700

    pcg code member

--- resources/views/pages/verification/detail.blade.php
+++ resources/views/pages/verification/detail.blade.php
@@ -499,6 +499,48 @@
                                 <div class="ln_solid"></div>
                             </div>
                         </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 ">
+                                <div class="x_panel">
+                                    <div class="x_title">
+                                        <h2>Review Fintech Data Center(FDC)</h2>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="x_content">
+
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="card-box table-responsive">
+                                                    <table id="history_table" class="table table-striped table-bordered" style="width:100%">
+                                                        <thead>
+                                                        <tr>
+                                                            <th>ID Pelanggan</th>
+                                                            <th>No KTP</th>
+                                                            <th>Nama</th>
+                                                            <th>Nilai Pinjaman</th>
+                                                            <th>Tanggal Pencairan</th>
+                                                            <th>Tanggal Jatuh Tempo</th>
+                                                            <th>DPD Terakhir</th>
+                                                            <th>DPD Max</th>
+                                                            <th>Sisa Pinjaman</th>
+                                                            <th>Kualitas Pinjaman</th>
+                                                            <th>Status Pinjaman</th>
+                                                        </tr>
+                                                        </thead>
+                                                        <tbody>
+
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="ln_solid"></div>
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-md-12 col-sm-12 ">
                                 <div class="x_panel">
